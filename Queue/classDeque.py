9d3ead8a443e906af6753132a29b5abc7717542f
Felipe Giraldo <astrxnomo@gmail.com>
2022-06-15 20:42:41 -0500

    Update classDeque.py

--- Queue/classDeque.py
+++ Queue/classDeque.py
@@ -1,21 +1,30 @@
-class ColaDoble:
+class Deque:
+    #Deque implementation as a list
+
     def __init__(self):
-        self.items = []
+        #Create new deque
+        self._items = []
 
-    def estaVacia(self):
-        return self.items == []
+    def is_empty(self):
+        #Check if the deque is empty
+        return not bool(self._items)
 
-    def agregarFrente(self, item):
-        self.items.append(item)
+    def add_front(self, item):
+        #Add an item to the front of the deque
+        self._items.append(item)
 
-    def agregarFinal(self, item):
-        self.items.insert(0,item)
+    def add_rear(self, item):
+        #Add an item to the rear of the deque
+        self._items.insert(0, item)
 
-    def removerFrente(self):
-        return self.items.pop()
+    def remove_front(self):
+        #Remove an item from the front of the deque
+        return self._items.pop()
 
-    def removerFinal(self):
-        return self.items.pop(0)
+    def remove_rear(self):
+        #Remove an item from the rear of the deque
+        return self._items.pop(0)
 
-    def tamano(self):
-        return len(self.items)
+    def size(self):
+        #Get the number of items in the deque
+        return len(self._items)
